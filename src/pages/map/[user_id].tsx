20a66f5ac0600191e1d8997eb949003990dfdb8a
Yoshino-Yukitaro <sushiki.az@gmail.com>
2023-01-31 23:53:34 +0900

    feat: とりあえず地図を表示した

--- src/pages/map/[user_id].tsx
+++ src/pages/map/[user_id].tsx
@@ -1,10 +1,33 @@
 import { Text } from "@chakra-ui/react"
+import { GoogleMap, useLoadScript } from "@react-google-maps/api"
+import { useCallback, useRef } from "react"
+
 import CommonLayout from "@/components/layouts/commonLayout"
 
+
+const mapContainerStyle = {
+    height: "60vh",
+    width: "100%",
+  }
+
 const UserMap = () => {
+    const  { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!,
+    })
+    const mapRef = useRef<google.maps.Map>()
+    const onMapLoad = useCallback((map: google.maps.Map) => {
+        mapRef.current = map;
+      }, []);
+
+    if(loadError) return "Error"
+    if(!isLoaded) return "Load中"
     return (
         <CommonLayout>
             <Text color="coconatsPink">サンプル</Text>
+            <GoogleMap id="map" mapContainerStyle={mapContainerStyle} zoom={8} center={{
+          lat: 43.048225,
+          lng: 141.49701,
+        }} onLoad={onMapLoad} />
         </CommonLayout>
     )
 }
