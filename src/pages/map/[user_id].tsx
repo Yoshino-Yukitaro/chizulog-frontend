6738a1024b146d485d125ac61fb8a7e2ee72925f
Yoshino-Yukitaro <sushiki.az@gmail.com>
2023-02-01 23:34:30 +0900

    feat: マーカーとマーカーをクリックした時のモーダルを追加

--- src/pages/map/[user_id].tsx
+++ src/pages/map/[user_id].tsx
@@ -1,34 +1,146 @@
-import { Text } from "@chakra-ui/react"
-import { GoogleMap, useLoadScript } from "@react-google-maps/api"
-import { useCallback, useRef } from "react"
+/** @format */
 
-import CommonLayout from "@/components/layouts/commonLayout"
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { GoogleMap, Marker, MarkerF, useLoadScript } from '@react-google-maps/api'
+import { useCallback, useRef } from 'react'
 
+import CommonLayout from '@/components/layouts/commonLayout'
 
 const mapContainerStyle = {
-    height: "60vh",
-    width: "100%",
-  }
+  height: '60vh',
+  width: '100%',
+}
 
 const UserMap = () => {
-    const  { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!,
-    })
-    const mapRef = useRef<google.maps.Map>()
-    const onMapLoad = useCallback((map: google.maps.Map) => {
-        mapRef.current = map;
-      }, []);
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!,
+    language: 'ja',
+  })
+  const mapRef = useRef<google.maps.Map>()
+  const onMapLoad = useCallback((map: google.maps.Map) => {
+    mapRef.current = map
+  }, [])
 
-    if(loadError) return "Error"
-    if(!isLoaded) return "Load中"
-    return (
-        <CommonLayout>
-            <Text color="coconatsPink">サンプル</Text>
-            <GoogleMap id="map" mapContainerStyle={mapContainerStyle} zoom={8} center={{
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const onLoad = (marker: google.maps.Marker) => {
+    console.log('marker: ', marker)
+  }
+
+  const onClick = (e: google.maps.MapMouseEvent) => {
+    alert('クリックされた')
+  }
+
+  if (loadError) return 'Error'
+  if (!isLoaded) return 'Load中'
+  return (
+    <CommonLayout>
+      <Text color='coconatsPink'>サンプル</Text>
+      <GoogleMap
+        id='map'
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={{
           lat: 43.048225,
           lng: 141.49701,
-        }} onLoad={onMapLoad} />
-        </CommonLayout>
-    )
+        }}
+        onLoad={onMapLoad}
+      >
+        <>
+          <MarkerF
+            position={{
+              lat: 43,
+              lng: 141,
+            }}
+            onLoad={onLoad}
+            onClick={onOpen}
+          />
+          <Modal
+            isCentered
+            isOpen={isOpen}
+            onClose={onClose}
+            scrollBehavior='inside'
+            size={{ base: 'xs', md: 'xl', lg: '2xl' }}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalCloseButton />
+              <ModalBody>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+                <p>あ</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button>追加</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      </GoogleMap>
+    </CommonLayout>
+  )
 }
 export default UserMap
