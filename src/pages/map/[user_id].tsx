48e04c08ba5313f072e6820007085f01061e003e
Yoshino-Yukitaro <sushiki.az@gmail.com>
2023-02-02 22:32:02 +0900

    feat: コンポーネントを整理した

--- src/pages/map/[user_id].tsx
+++ src/pages/map/[user_id].tsx
@@ -18,14 +18,19 @@ import {
   VStack,
 } from '@chakra-ui/react'
 import { GoogleMap, Marker, MarkerF, useLoadScript } from '@react-google-maps/api'
+import { DateTime } from 'luxon'
 import { useCallback, useRef } from 'react'
 
 import CommonLayout from '@/components/layouts/commonLayout'
+import LogCard from '@/components/parts/map/logCard'
+import MemoryMarker from '@/components/parts/map/memoryMarker'
 
 const mapContainerStyle = {
   height: '60vh',
   width: '100%',
 }
+
+const datetime = DateTime.now()
 
 const UserMap = () => {
   const { isLoaded, loadError } = useLoadScript({
@@ -36,16 +41,6 @@ const UserMap = () => {
   const onMapLoad = useCallback((map: google.maps.Map) => {
     mapRef.current = map
   }, [])
-
-  const { isOpen, onOpen, onClose } = useDisclosure()
-
-  const onLoad = (marker: google.maps.Marker) => {
-    console.log('marker: ', marker)
-  }
-
-  const onClick = (e: google.maps.MapMouseEvent) => {
-    alert('クリックされた')
-  }
 
   if (loadError) return 'Error'
   if (!isLoaded) return 'Load中'
@@ -62,126 +57,8 @@ const UserMap = () => {
         }}
         onLoad={onMapLoad}
       >
-        <>
-          <MarkerF
-            position={{
-              lat: 43,
-              lng: 141,
-            }}
-            onLoad={onLoad}
-            onClick={onOpen}
-          />
-          <Modal
-            isCentered
-            isOpen={isOpen}
-            onClose={onClose}
-            scrollBehavior='inside'
-            size={{ base: 'xs', md: 'xl', lg: '2xl' }}
-          >
-            <ModalOverlay />
-            <ModalContent>
-              <ModalCloseButton />
-              <ModalBody>
-                <VStack marginTop='50px'>
-                  <Card w={{ base: '98%', md: '90%' }} direction='row'>
-                    <Image
-                      objectFit='cover'
-                      boxSize={{ base: '100px', md: '150px' }}
-                      src=''
-                      fallbackSrc='https://via.placeholder.com/150'
-                      alt='Caffe Latte'
-                    />
-                    <VStack alignItems='start'>
-                      <CardBody padding={{ base: '8px' }}>
-                        <Text as='h2' fontSize={{ base: 'xl', md: '2xl' }}>
-                          タイトル
-                        </Text>
-                      </CardBody>
-                      <CardFooter padding={{ base: '8px' }}>
-                        <Text>2023年2月2日</Text>
-                      </CardFooter>
-                    </VStack>
-                  </Card>
-                  <Card w={{ base: '98%', md: '90%' }} direction='row'>
-                    <Image
-                      objectFit='cover'
-                      boxSize={{ base: '100px', md: '150px' }}
-                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-                      alt='Caffe Latte'
-                    />
-                    <VStack alignItems='start'>
-                      <CardBody padding={{ base: '8px' }}>
-                        <Text as='h2' fontSize={{ base: 'xl', md: '2xl' }}>
-                          タイトル
-                        </Text>
-                      </CardBody>
-                      <CardFooter padding={{ base: '8px' }}>
-                        <Text>2023年2月2日</Text>
-                      </CardFooter>
-                    </VStack>
-                  </Card>
-                  <Card w={{ base: '98%', md: '90%' }} direction='row'>
-                    <Image
-                      objectFit='cover'
-                      maxW={{ base: '100px', md: '200px' }}
-                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-                      alt='Caffe Latte'
-                    />
-                    <VStack alignItems='start'>
-                      <CardBody padding={{ base: '8px' }}>
-                        <Text as='h2' fontSize={{ base: 'xl', md: '2xl' }}>
-                          タイトル
-                        </Text>
-                      </CardBody>
-                      <CardFooter padding={{ base: '8px' }}>
-                        <Text>2023年2月2日</Text>
-                      </CardFooter>
-                    </VStack>
-                  </Card>
-                  <Card w={{ base: '98%', md: '90%' }} direction='row'>
-                    <Image
-                      objectFit='cover'
-                      maxW={{ base: '100px', md: '200px' }}
-                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-                      alt='Caffe Latte'
-                    />
-                    <VStack alignItems='start'>
-                      <CardBody padding={{ base: '8px' }}>
-                        <Text as='h2' fontSize={{ base: 'xl', md: '2xl' }}>
-                          タイトル
-                        </Text>
-                      </CardBody>
-                      <CardFooter padding={{ base: '8px' }}>
-                        <Text>2023年2月2日</Text>
-                      </CardFooter>
-                    </VStack>
-                  </Card>
-                  <Card w={{ base: '98%', md: '90%' }} direction='row'>
-                    <Image
-                      objectFit='cover'
-                      maxW={{ base: '100px', md: '200px' }}
-                      src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
-                      alt='Caffe Latte'
-                    />
-                    <VStack alignItems='start'>
-                      <CardBody padding={{ base: '8px' }}>
-                        <Text as='h2' fontSize={{ base: 'xl', md: '2xl' }}>
-                          タイトル
-                        </Text>
-                      </CardBody>
-                      <CardFooter padding={{ base: '8px' }}>
-                        <Text>2023年2月2日</Text>
-                      </CardFooter>
-                    </VStack>
-                  </Card>
-                </VStack>
-              </ModalBody>
-              <ModalFooter>
-                <Button>追加</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        </>
+        <MemoryMarker lat={43} lng={141} />
+        <MemoryMarker lat={44} lng={142} />
       </GoogleMap>
     </CommonLayout>
   )
