4c46a73f90a99fdb7e3b2fedbfb7ae7d878ad0b9
Yoshino-Yukitaro <sushiki.az@gmail.com>
2023-02-03 23:48:47 +0900

    feat: 位置情報取得機能を追加

--- src/pages/map/[user_id].tsx
+++ src/pages/map/[user_id].tsx
@@ -6,6 +6,8 @@ import {
   CardBody,
   CardFooter,
   CardHeader,
+  Flex,
+  HStack,
   Image,
   Modal,
   ModalBody,
@@ -13,13 +15,15 @@ import {
   ModalContent,
   ModalFooter,
   ModalOverlay,
+  position,
+  Spacer,
   Text,
   useDisclosure,
   VStack,
 } from '@chakra-ui/react'
 import { GoogleMap, Marker, MarkerF, useLoadScript } from '@react-google-maps/api'
 import { DateTime } from 'luxon'
-import { useCallback, useRef } from 'react'
+import { MouseEvent, MouseEventHandler, useCallback, useRef, useState } from 'react'
 
 import CommonLayout from '@/components/layouts/commonLayout'
 import LogCard from '@/components/parts/map/logCard'
@@ -37,16 +41,23 @@ const UserMap = () => {
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!,
     language: 'ja',
   })
+  const [coordinates, setCoordinates] = useState<GeolocationPosition>()
   const mapRef = useRef<google.maps.Map>()
   const onMapLoad = useCallback((map: google.maps.Map) => {
     mapRef.current = map
   }, [])
 
+  const getCoordinates = (e: MouseEvent<HTMLButtonElement>) => {
+    navigator.geolocation.getCurrentPosition(setCoordinates)
+  }
+
   if (loadError) return 'Error'
   if (!isLoaded) return 'Load中'
   return (
     <CommonLayout>
-      <Text color='coconatsPink'>サンプル</Text>
+      <Text color='coconatsPink' fontSize='3xl' paddingY='15px'>
+        地図のタイトル
+      </Text>
       <GoogleMap
         id='map'
         mapContainerStyle={mapContainerStyle}
@@ -58,7 +69,18 @@ const UserMap = () => {
         onLoad={onMapLoad}
       >
         <MemoryMarker lat={43} lng={141} />
-        <MemoryMarker lat={44} lng={142} />
+        <MemoryMarker
+          lat={coordinates?.coords.latitude ? coordinates?.coords.latitude : 42}
+          lng={coordinates?.coords.longitude ? coordinates?.coords.longitude : 142}
+        />
+        <VStack h='100%' w='100%' padding='25px 55px'>
+          <Spacer />
+          <HStack w='100%'>
+            <Spacer />
+            <Button>追加</Button>
+            <Button onClick={getCoordinates}>スナップ</Button>
+          </HStack>
+        </VStack>
       </GoogleMap>
     </CommonLayout>
   )
