d3c62ef4a84532b47235dd2db017feffb066af8e
Yoshino-Yukitaro <sushiki.az@gmail.com>
2023-02-06 23:44:11 +0900

    feat: ヘッダーにボタンを追加

--- src/components/parts/commons/header/header.tsx
+++ src/components/parts/commons/header/header.tsx
@@ -1,14 +1,16 @@
-import { Avatar, HStack, Spacer, Text } from "@chakra-ui/react"
-import { useState } from "react"
+/** @format */
+
+import { HStack, Spacer } from '@chakra-ui/react'
+import FunctionalButton from './functionalButton'
+import Logo from './logo'
 
 const Header = () => {
-    const [isLogin, setInsLogin] = useState(false);
-    return (
-        <HStack bgColor="poolBlue" w="100vw" h="50px" padding="30px 20px">
-            <Text fontSize="3xl" fontWeight="bold" color="mashmallowMint" fontFamily="m_plus2">ちずろぐ</Text>
-            <Spacer />
-            <Avatar />
-        </HStack>
-    )
+  return (
+    <HStack bgColor='poolBlue' w='100vw' h='50px' padding='30px 20px'>
+      <Logo />
+      <Spacer />
+      <FunctionalButton />
+    </HStack>
+  )
 }
 export default Header
